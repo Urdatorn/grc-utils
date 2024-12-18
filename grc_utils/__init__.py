abe044c206962ef207ab1f7f102837ff243b70db
Urdatorn <urdatorn@hotmail.com>
2024-12-18 15:01:54 +0700

    remov init from gitignore

---
+++ grc_utils/__init__.py
@@ -0,0 +1,8 @@
+# grc_utils/__init__.py
+
+from .dichrona import *
+from .erics_syllabifier import *
+from .filter_dichrona import *
+from .longa import *
+from .macrons_map import *
+from .utils import *
